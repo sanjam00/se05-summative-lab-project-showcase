176539480d34bebf4e187da9ce6e6fe6952ec9db
Sanaya James <sanaya.j.james@gmail.com>
2025-12-17 18:35:37 -0700

    created NewTeaForm, rendered in AdminPage

---
+++ src/components/NewTeaForm.jsx
@@ -0,0 +1,20 @@
+export default function NewTeaForm() {
+  return (
+    <form>
+      <label htmlFor="name-input">Name:</label>
+      <input id="name-input" type="textbox" />
+
+      <label htmlFor="image-input">Image:</label>
+      <input id="image-input" type="textbox" />
+
+      <label htmlFor="description-input">Description:</label>
+      <input id="description-input" type="textbox" />
+
+      <label htmlFor="caffeinated-checkbox">Caffeinated:</label>
+      <input id="caffeinated-checkbox" type="checkbox" />
+
+      <label htmlFor="price-input">Price:</label>
+      <input id="price-input" type="textbox" />
+    </form>
+  )
+}
--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,8 +1,13 @@
+import NewTeaForm from "../components/NewTeaForm";
+
 export default function AdminPage() {
   return (
     <>
       <h1>This is the Admin Portal Page</h1>
       <p>Here, an admin can edit products</p>
+      {/* render a login functionality */}
+      {/* render the new tea form */}
+      <NewTeaForm />
     </>
   )
 }
